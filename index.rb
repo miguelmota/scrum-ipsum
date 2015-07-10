94f816f49249b50734070a6cb50b3383c50e5bda
Miguel Mota <miguelmota2@gmail.com>
2015-07-10 13:23:08 -0700

    dir

--- index.rb
+++ index.rb
@@ -1,10 +1,9 @@
 require 'marky_markov'
-require 'pathname'
 
 markov = MarkyMarkov::TemporaryDictionary.new
-markov.parse_file Pathname.new "train/phrases.txt"
-markov.parse_file Pathname.new "train/agile-glossary.txt"
-markov.parse_file Pathname.new "train/scrumalliance.txt"
+markov.parse_file Dir.pwd + "/train/phrases.txt"
+markov.parse_file Dir.pwd + "/train/agile-glossary.txt"
+markov.parse_file Dir.pwd + "/train/scrumalliance.txt"
 puts markov.generate_n_sentences 10
 puts markov.generate_n_sentences 10
 puts markov.generate_n_sentences 5
